cc69a9f1d335106b83ed988a3ab38df90d81ae45
Steven <stevenxia@protonmail.com>
2019-04-21 18:31:00 -0700

    Beautify !eval command output.
    And also bump version.

--- lichess-bot.py
+++ lichess-bot.py
@@ -27,7 +27,7 @@ try:
 except ImportError:
     from http.client import BadStatusLine as RemoteDisconnected
 
-__version__ = "1.1.6 [unofficial]"
+__version__ = "1.1.7 [unofficial]"
 
 terminated = False
 
